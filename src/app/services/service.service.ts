60faac2aa89355f4982ae403ea883ef37a0b3a4f
ayoubelhafi <ucseme@gmail.com>
2019-10-17 14:23:27 +0100

    service_service

---
+++ src/app/services/service.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Service } from '../models/service.model';
+import { HttpClient } from '@angular/common/http';
+import { TreeError } from '@angular/compiler';
+import { stringify } from 'querystring';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+  url:string="http://localhost:52255/api/services";
+  services:Service[];
+  service:Service;
+  constructor(private http:HttpClient) {}
+
+    getAllServices(){
+      this.http.get(this.url).toPromise().then(
+        res=>{
+          this.services = res as Service[];
+        }
+      )
+    }
+  
+    postService(){
+      return this.http.post(this.url,this.service);
+    }
+  
+    putService(){
+      return this.http.put(this.url + "/" + this.service.id,this.service);
+    }
+  
+    deleteService(id){
+      return this.http.delete(this.url+"/"+id);
+    }
+   }
