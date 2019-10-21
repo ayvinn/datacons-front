cd05193e139a784ce2b6dbce37bbcb95c45af8f9
ayoubelhafi <ucseme@gmail.com>
2019-10-21 16:38:12 +0100

    routingconfig1

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SeviceComponent } from '../component/sevice/sevice.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path:"service",
+      component:SeviceComponent,
+      pathMatch :'full'
+
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
