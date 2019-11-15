9c6dc362c5e56c681ae38daa3d14462aeb241173
Mohammed JELLAB <ats.medjellab@gmail.com>
2019-11-15 17:34:57 +0100

    edit responsable component

--- src/app/component/consignation/add-consignation/responsables/responsables.component.ts
+++ src/app/component/consignation/add-consignation/responsables/responsables.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ServicedemandeurService } from 'src/app/services/servicedemandeur.service';
+import { MatStepper } from '@angular/material';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-responsables',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponsablesComponent implements OnInit {
 
-  constructor() { }
+  @Input() stepper: MatStepper;
+  responsableForm: FormGroup;
+
+  charges = [];
+  elecs = [];
+  mecs = [];
+
+  constructor(private demandeurService: ServicedemandeurService,
+    private formBuilder: FormBuilder,
+    private dataService: DataService
+    ) { }
 
   ngOnInit() {
+    this.dataService.allDataConsignation.subscribe();
+    this.responsableForm = this.formBuilder.group({
+      idDemandeurCP: ['', Validators.required],
+      idDemandeurMec: [''],
+      idDemandeurElec: ['']
+    });
+    this.demandeurService.getAllDemandeurs().subscribe((res: []) => { 
+
+      console.log('Demandeurs: ', res);
+      res.forEach(element => {
+        console.log('Element: ', element['idcategorieNavigation']['nomcomplet']);
+        if(element['idcategorieNavigation']['nomcomplet'] === 'ELEC') {
+          this.elecs.push(element);
+        }
+
+        if(element['idcategorieNavigation']['nomcomplet'] === 'Opérateur' || element['idcategorieNavigation']['nomComplet'] === 'Chef de poste') {
+          this.charges.push(element);
+        }
+
+        if(element['idcategorieNavigation']['nomcomplet'] === 'MEC') {
+          this.mecs.push(element);
+        }
+      });
+      console.log('Charges: ', this.charges);
+      console.log('elecs: ', this.elecs);
+      console.log('mecs: ', this.mecs);
+    });
+  }
+
+  goPrevious() {
+    this.stepper.previous();
+  }
+
+  change(text, type) {
+    const data = {};
+    data[type] = text;
+    this.dataService.changeConsignation(data);
   }
 
 }
