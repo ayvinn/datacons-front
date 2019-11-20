b142d1cfba57edb54e68e0ff8db114e072056985
Mohammed JELLAB <ats.medjellab@gmail.com>
2019-11-20 11:31:03 +0100

    add list of consignation

--- src/app/component/consignation/consignation.component.ts
+++ src/app/component/consignation/consignation.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ConsignationService } from 'src/app/services/consignation.service';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-consignation',
   templateUrl: './consignation.component.html',
   styleUrls: ['./consignation.component.sass']
 })
-export class ConsignationComponent implements OnInit {
+export class ConsignationComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+
+  displayedColumns: string[] = ['id', 'description', 'duree', 'demandeur', 'secteur', 'action'];
+  dataSource;
+
+
+  constructor(private consignationService: ConsignationService) { }
 
   ngOnInit() {
+    
+  }
+
+  ngAfterViewInit() {
+    const etat = true;
+    this.consignationService.getConsignation(etat).subscribe((res:any) => {
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      // console.log('Get Consignation: ', res);
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    // console.log('Filter Value: ', filterValue);
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
