d3c265e7b2bc25d6dd51bf7b9c8a00dfc40f5480
ATS Abderrahmane <ats.abessalhi@gmail.com>
2019-12-10 10:14:23 +0100

    IP edit

--- src/app/shared/constantURL.ts
+++ src/app/shared/constantURL.ts
@@ -1,6 +1,5 @@
 export const constantURL = {
-    //apiEndpoint:'http://10.54.5.29:1050'
-    apiEndpoint:'http://192.168.1.88:5000', 
-    // apiEndpoint:'http://10.54.6.234:1050',
-    // apiEndpoint:'http://localhost:52255',
+    // apiEndpoint:'http://192.168.1.88:5000', 
+    apiEndpoint:'http://10.54.6.234:1050',// site nador
+    // apiEndpoint:'http://10.54.5.29:1050',// site oujda
 }; 
